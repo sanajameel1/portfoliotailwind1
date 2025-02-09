7f6bcab6a16f73946de3fe64ee0a6cde46682d64
sana <161457792+sanajameel1@users.noreply.github.com>
2025-02-10 00:02:18 +0500

    Update page.tsx

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -10,17 +10,17 @@ import Image from 'next/image';
 //         <div className="flex flex-col flex-1 gap-4 justify-center p-6 text-center md:text-left">
 //           <h1 className="font-bold text-3xl mt-4 sm:text-4xl md:text-5xl">Sana Jameel</h1>
 //           <p className="font-normal text-sm sm:text-base md:text-lg">
-//             Hi, I`m Sana Jameel, a highly skilled web developer with extensive
+//             Hi I m Sana Jameel a highly skilled web developer with extensive
 //             expertise in creating dynamic and responsive web applications.
 //             <br />
 //             <br />
-//             Currently, I`m part of an amazing training program at the Governor
+//             Currently I m part of an amazing training program at the Governor
 //             House, which has helped me grow tremendously. This program is guided
 //             by some of the most incredible mentors.
 //             <br />
 //             <br />
-//             In addition to my web development skills, I have strong command over
-//             programming languages such as HTML, CSS, JavaScript, TypeScript, and
+//             In addition to my web development skills I have strong command over
+//             programming languages such as HTML CSS JavaScript TypeScript and
 //             Next.js.
 //           </p>
 //         </div>
@@ -34,8 +34,8 @@ export default function AboutPage() {
       <div className="max-w-3xl bg-white shadow-lg rounded-lg p-8 border border-gray-800">
         <h1 className="text-4xl font-bold text-center text-blue-700">About Me</h1>
         <p className="mt-4 text-lg text-gray-700 text-center">
-          I am a dedicated IT student at Governor House, passionate about web development and modern technologies. 
-          My focus is on creating innovative, user-friendly, and responsive web applications.
+          I am a dedicated IT student at Governor House passionate about web development and modern technologies. 
+          My focus is on creating innovative user-friendly and responsive web applications.
         </p>
 
         <div className="mt-6 flex justify-center">
@@ -49,13 +49,13 @@ export default function AboutPage() {
 </div>
         <div className="mt-6 text-center">
           <p className="text-lg text-gray-600 italic">
-            "Innovation and technology drive the future, and I am here to shape it!"
+            Innovation and technology drive the future, and I am here to shape it
           </p>
         </div>
 
         {/* <div className="mt-6 text-center">
           <a
-            href="/portfolio"
+            href
             className="px-6 py-2 text-white bg-green-700 rounded-full shadow hover:bg-green-800 transition"
           >
             View My Work
